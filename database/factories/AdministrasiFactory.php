296bdde1e116bf2539e6d2236147e4700c76d721
Muhammad Ilham Dimas Meidi <8040210230@student.unama.ac.id>
2026-07-27 12:30:13 +0700

    Add Laravel factories and seeders for bulk dummy data

---
+++ database/factories/AdministrasiFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Administrasi;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Administrasi>
+ */
+class AdministrasiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
